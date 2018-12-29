0bd0004beab5beb2f65ec9c9000b1018dc0562fc
Eran Hirsch <hirsch@fb.com>
2018-12-29 17:32:38 +0200

    typo in selector

--- scyther.js
+++ scyther.js
@@ -129,7 +129,7 @@ function renderBoardSelection(boardSelection, proximity) {
 
 function getPlayerCount() {
   var selector = "input[name='" + PLAYER_COUNT_GROUP_NAME + "']";
-  return $(selector + ':active').val() || $(select + ':checked').val();
+  return $(selector + ':active').val() || $(selector + ':checked').val();
 }
 
 function renderBoards() {
